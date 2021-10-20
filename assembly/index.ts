b0935c356dbe562b31349f9a5c56d3431f43c45b
ashutosh1206 <ashutosh.ahelleya@gmail.com>
2021-10-20 14:58:50 +0530

    Store capsule usernames and public keys on NEAR

--- assembly/index.ts
+++ assembly/index.ts
@@ -1,10 +1,21 @@
-import { Context, storage } from 'near-sdk-as'
+import { Context, storage } from "near-sdk-as";
 
-export function getProfile(accountId: string): string | null {
-	return storage.get<string>(accountId, 'Hello')
+export function setUserInfo(username: string, pubkey: string): void {
+	const arr = new Array<string>(2);
+	arr[0] = Context.sender;
+	arr[1] = pubkey;
+
+	const val = storage.get<Array<string>>(username);
+	if (!val) {
+		storage.set<Array<string>>(username, arr);
+		return;
+	}
+
+	if (val[0] == username) {
+		storage.set<Array<string>>(username, arr);
+	}
 }
 
-export function setProfile(cid: string): void {
-	const accountId = Context.sender
-	storage.set(accountId, cid)
+export function getUserInfo(username: string): Array<string> | null {
+	return storage.get<Array<string>>(username);
 }
