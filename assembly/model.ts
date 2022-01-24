74af393d7a70f368ddddf213a794ded59bcc7c21
ashutosh1206 <ashutosh.ahelleya@gmail.com>
2022-01-25 00:20:41 +0530

    Add support to blocklist

--- assembly/model.ts
+++ assembly/model.ts
@@ -3,6 +3,7 @@ import { PersistentMap } from "near-sdk-as";
 export const userLookup = new PersistentMap<string, Array<string>>("u");
 export const accountLookup = new PersistentMap<string, string>("a");
 export const onboardLookup = new PersistentMap<string, bool>("o");
-export const blockList = new Set<string>()
-blockList.add("root")
-blockList.add("admin")
+export const blockList = new Set<string>();
+blockList.add("root");
+blockList.add("admin");
+blockList.add("support");
