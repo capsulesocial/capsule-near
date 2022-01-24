e3f5e740823dcd361fbe6a50fa9038944ea3f00b
ashutosh1206 <ashutosh.ahelleya@gmail.com>
2022-01-24 15:17:37 +0530

    Allow support as username

--- assembly/model.ts
+++ assembly/model.ts
@@ -5,5 +5,4 @@ export const accountLookup = new PersistentMap<string, string>("a");
 export const onboardLookup = new PersistentMap<string, bool>("o");
 export const blockList = new Set<string>()
 blockList.add("root")
-blockList.add("support")
 blockList.add("admin")
