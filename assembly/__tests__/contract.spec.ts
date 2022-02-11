bf686e0216f64149d379a098d8f87a199fe67c21
Ashutosh Ahelleya <ashutosh.ahelleya@gmail.com>
2022-02-11 20:47:46 +0530

    Allow capsule accounts to register using blocklisted usernames (#24)
    * Add .vscode to gitignore
    * Allow blocklisted usernames to register
    * Update README
    * Add basic tests for blocklisted-user registration
    * Add meaning of each return code

--- assembly/__tests__/contract.spec.ts
+++ assembly/__tests__/contract.spec.ts
@@ -1,9 +1,25 @@
 import { VMContext } from "near-sdk-as";
-import { onboardAccount, setUserInfo } from "..";
-import { accountLookup, onboardLookup, userLookup } from "../model";
+import {
+	onboardAccount,
+	requestSetUserInfo,
+	setUserInfo,
+	verifySetUserInfo,
+} from "..";
+import {
+	accountLookup,
+	onboardLookup,
+	userLookup,
+	userRequestLookup,
+} from "../model";
 
 const inputAccountId: string = "onboard.testnet";
 const inputUsername: string = "testuser";
+
+const blistedAccountId: string = "onboard.testnet";
+const blistedUserOne: string = "root";
+const blistedUserTwo: string = "admin";
+const blistedUserThree: string = "support";
+const blistedUserCapsule: string = "capsuleoff";
 
 describe("onboard test", () => {
 	afterEach(() => {
@@ -188,5 +204,284 @@ describe("registration test", () => {
 		if (result) {
 			expect(result[0]).toBe(inputAccountId);
 		}
+	});
+});
+
+describe("testing registration of blocklisted usernames", () => {
+	afterEach(() => {
+		onboardLookup.delete(blistedAccountId);
+		onboardLookup.delete("onboard2.testnet");
+
+		accountLookup.delete(blistedAccountId);
+
+		userRequestLookup.delete(inputUsername);
+		userRequestLookup.delete(blistedUserCapsule);
+		userRequestLookup.delete(blistedUserOne);
+		userRequestLookup.delete(blistedUserTwo);
+		userRequestLookup.delete(blistedUserThree);
+
+		userLookup.delete(inputUsername);
+		userLookup.delete(blistedUserCapsule);
+		userLookup.delete(blistedUserOne);
+		userLookup.delete(blistedUserTwo);
+		userLookup.delete(blistedUserThree);
+	});
+
+	it("should return an error: accountId not onboarded", () => {
+		VMContext.setSigner_account_id(blistedAccountId);
+		expect(requestSetUserInfo(blistedUserOne)).toBe(6);
+
+		expect(userRequestLookup.contains(blistedUserOne)).toBe(false);
+		expect(userLookup.contains(blistedUserOne)).toBe(false);
+		expect(accountLookup.contains(blistedAccountId)).toBe(false);
+	});
+
+	it("should return an error: username is too small", () => {
+		VMContext.setSigner_account_id("capsule.testnet");
+		onboardAccount(blistedAccountId);
+
+		VMContext.setSigner_account_id(blistedAccountId);
+		expect(requestSetUserInfo("ab")).toBe(2);
+
+		expect(userRequestLookup.contains("ab")).toBe(false);
+		expect(accountLookup.contains(blistedAccountId)).toBe(false);
+		expect(userLookup.contains("ab")).toBe(false);
+	});
+
+	it("should return an error: username is too long", () => {
+		VMContext.setSigner_account_id("capsule.testnet");
+		onboardAccount(blistedAccountId);
+
+		VMContext.setSigner_account_id(blistedAccountId);
+		expect(requestSetUserInfo("abcdefghijklmnopqrs")).toBe(4);
+
+		expect(userRequestLookup.contains("abcdefghijklmnopqrs")).toBe(false);
+		expect(accountLookup.contains(blistedAccountId)).toBe(false);
+		expect(userLookup.contains("abcdefghijklmnopqrs")).toBe(false);
+	});
+
+	it("should return an error: username cannot contain uppercase characters", () => {
+		VMContext.setSigner_account_id("capsule.testnet");
+		onboardAccount(blistedAccountId);
+
+		VMContext.setSigner_account_id(blistedAccountId);
+		expect(requestSetUserInfo("tesT")).toBe(8);
+
+		expect(userRequestLookup.contains("tesT")).toBe(false);
+		expect(accountLookup.contains(blistedAccountId)).toBe(false);
+		expect(userLookup.contains("tesT")).toBe(false);
+	});
+
+	it("should return an error: username cannot contain dots", () => {
+		VMContext.setSigner_account_id("capsule.testnet");
+		onboardAccount(blistedAccountId);
+
+		VMContext.setSigner_account_id(blistedAccountId);
+		expect(requestSetUserInfo("tes.")).toBe(8);
+
+		expect(userRequestLookup.contains("tes.")).toBe(false);
+		expect(accountLookup.contains(blistedAccountId)).toBe(false);
+		expect(userLookup.contains("tes.")).toBe(false);
+	});
+
+	it("should return an error: username is not blocklisted", () => {
+		VMContext.setSigner_account_id("capsule.testnet");
+		onboardAccount(blistedAccountId);
+
+		VMContext.setSigner_account_id(blistedAccountId);
+		expect(requestSetUserInfo(inputUsername)).toBe(7);
+
+		expect(userRequestLookup.contains(inputUsername)).toBe(false);
+		expect(accountLookup.contains(blistedAccountId)).toBe(false);
+		expect(userLookup.contains(inputUsername)).toBe(false);
+	});
+
+	it("should return an error: accountId linked to another registered username", () => {
+		VMContext.setSigner_account_id("capsule.testnet");
+		onboardAccount(blistedAccountId);
+
+		VMContext.setSigner_account_id(blistedAccountId);
+		setUserInfo(inputUsername);
+
+		expect(requestSetUserInfo(blistedUserOne)).toBe(5);
+	});
+
+	it("should successfully add 'root' to waiting list", () => {
+		VMContext.setSigner_account_id("capsule.testnet");
+		onboardAccount(blistedAccountId);
+
+		VMContext.setSigner_account_id(blistedAccountId);
+		expect(requestSetUserInfo(blistedUserOne)).toBe(1);
+
+		expect(userRequestLookup.contains(blistedUserOne)).toBe(true);
+		const result = userRequestLookup.get(blistedUserOne);
+		if (result) {
+			expect(result[0]).toBe(blistedAccountId);
+		}
+		expect(userLookup.contains(blistedUserOne)).toBe(false);
+		expect(accountLookup.contains(blistedAccountId)).toBe(false);
+	});
+
+	it("should successfully add 'admin' to waiting list", () => {
+		VMContext.setSigner_account_id("capsule.testnet");
+		onboardAccount(blistedAccountId);
+
+		VMContext.setSigner_account_id(blistedAccountId);
+		expect(requestSetUserInfo(blistedUserTwo)).toBe(1);
+
+		expect(userRequestLookup.contains(blistedUserTwo)).toBe(true);
+		const result = userRequestLookup.get(blistedUserOne);
+		if (result) {
+			expect(result[0]).toBe(blistedAccountId);
+		}
+		expect(userLookup.contains(blistedUserTwo)).toBe(false);
+		expect(accountLookup.contains(blistedAccountId)).toBe(false);
+	});
+
+	it("should successfully add 'support' to waiting list", () => {
+		VMContext.setSigner_account_id("capsule.testnet");
+		onboardAccount(blistedAccountId);
+
+		VMContext.setSigner_account_id(blistedAccountId);
+		expect(requestSetUserInfo(blistedUserThree)).toBe(1);
+
+		expect(userRequestLookup.contains(blistedUserThree)).toBe(true);
+		const result = userRequestLookup.get(blistedUserOne);
+		if (result) {
+			expect(result[0]).toBe(blistedAccountId);
+		}
+		expect(userLookup.contains(blistedUserThree)).toBe(false);
+		expect(accountLookup.contains(blistedAccountId)).toBe(false);
+	});
+
+	it("should successfully add 'capsuleoff' to waiting list", () => {
+		VMContext.setSigner_account_id("capsule.testnet");
+		onboardAccount(blistedAccountId);
+
+		VMContext.setSigner_account_id(blistedAccountId);
+		expect(requestSetUserInfo(blistedUserCapsule)).toBe(1);
+
+		expect(userRequestLookup.contains(blistedUserCapsule)).toBe(true);
+		const result = userRequestLookup.get(blistedUserOne);
+		if (result) {
+			expect(result[0]).toBe(blistedAccountId);
+		}
+		expect(userLookup.contains(blistedUserCapsule)).toBe(false);
+		expect(accountLookup.contains(blistedAccountId)).toBe(false);
+	});
+
+	it("should add two usernames to waiting list with same accountId", () => {
+		VMContext.setSigner_account_id("capsule.testnet");
+		onboardAccount(blistedAccountId);
+
+		VMContext.setSigner_account_id(blistedAccountId);
+		requestSetUserInfo(blistedUserOne);
+
+		expect(requestSetUserInfo(blistedUserTwo)).toBe(1);
+		expect(userRequestLookup.contains(blistedUserTwo)).toBe(true);
+		const result = userRequestLookup.get(blistedUserTwo);
+		if (result) {
+			expect(result[0]).toBe(blistedAccountId);
+		}
+	});
+
+	it("should return an error: username is already in waiting list", () => {
+		VMContext.setSigner_account_id("capsule.testnet");
+		onboardAccount(blistedAccountId);
+		onboardAccount("onboard2.testnet");
+
+		VMContext.setSigner_account_id(blistedAccountId);
+		requestSetUserInfo(blistedUserOne);
+
+		VMContext.setSigner_account_id("onboard2.testnet");
+		expect(requestSetUserInfo(blistedUserOne)).toBe(3);
+		expect(userRequestLookup.contains(blistedUserOne)).toBe(true);
+		const result = userRequestLookup.get(blistedUserOne);
+		if (result) {
+			expect(result[0]).toBe(blistedAccountId);
+		}
+	});
+
+	it("[verify] should return an error: only admin accounts allowed to verify", () => {
+		VMContext.setSigner_account_id("capsule.testnet");
+		onboardAccount(blistedAccountId);
+
+		VMContext.setSigner_account_id(blistedAccountId);
+		requestSetUserInfo(blistedUserOne);
+
+		expect(verifySetUserInfo(blistedUserOne)).toBe(0);
+
+		expect(userRequestLookup.contains(blistedUserOne)).toBe(true);
+		expect(userLookup.contains(blistedUserOne)).toBe(false);
+		expect(accountLookup.contains(blistedAccountId)).toBe(false);
+	});
+
+	it("[verify] should return an error: username not in waiting list", () => {
+		VMContext.setSigner_account_id("capsule.testnet");
+		expect(verifySetUserInfo(blistedUserOne)).toBe(2);
+
+		expect(userRequestLookup.contains(blistedUserOne)).toBe(false);
+		expect(userLookup.contains(blistedUserOne)).toBe(false);
+		expect(accountLookup.contains(blistedAccountId)).toBe(false);
+	});
+
+	it("[verify] should register a blocklisted username", () => {
+		VMContext.setSigner_account_id("capsule.testnet");
+		onboardAccount(blistedAccountId);
+
+		VMContext.setSigner_account_id(blistedAccountId);
+		requestSetUserInfo(blistedUserOne);
+
+		expect(userRequestLookup.contains(blistedUserOne)).toBe(true);
+		const userRequestInfo = userRequestLookup.get(blistedUserOne);
+
+		VMContext.setSigner_account_id("capsule.testnet");
+		expect(verifySetUserInfo(blistedUserOne)).toBe(1);
+
+		expect(userRequestLookup.contains(blistedUserOne)).toBe(false);
+
+		expect(userLookup.contains(blistedUserOne)).toBe(true);
+		const userInfo = userLookup.get(blistedUserOne);
+
+		expect(accountLookup.contains(blistedAccountId)).toBe(true);
+		expect(accountLookup.get(blistedAccountId)).toBe(blistedUserOne);
+
+		// Always true
+		if (userInfo && userRequestInfo) {
+			expect(userInfo[0]).toBe(userRequestInfo[0]);
+			expect(userInfo[1]).toBe(userRequestInfo[1]);
+		}
+	});
+
+	it("[verify] should return an error: accountId already linked to another username", () => {
+		VMContext.setSigner_account_id("capsule.testnet");
+		onboardAccount(blistedAccountId);
+
+		VMContext.setSigner_account_id(blistedAccountId);
+		requestSetUserInfo(blistedUserOne);
+		requestSetUserInfo(blistedUserTwo);
+
+		VMContext.setSigner_account_id("capsule.testnet");
+		verifySetUserInfo(blistedUserOne);
+		expect(verifySetUserInfo(blistedUserTwo)).toBe(5);
+
+		expect(userLookup.contains(blistedUserTwo)).toBe(false);
+		expect(userRequestLookup.contains(blistedUserTwo)).toBe(false);
+		expect(userLookup.contains(blistedUserOne)).toBe(true);
+	});
+
+	it("should return an error: username is already a registered user", () => {
+		VMContext.setSigner_account_id("capsule.testnet");
+		onboardAccount(blistedAccountId);
+		onboardAccount("onboard2.testnet");
+
+		VMContext.setSigner_account_id(blistedAccountId);
+		requestSetUserInfo(blistedUserOne);
+		VMContext.setSigner_account_id("capsule.testnet");
+		verifySetUserInfo(blistedUserOne);
+
+		VMContext.setSigner_account_id("onboard2.testnet");
+		expect(requestSetUserInfo(blistedUserOne)).toBe(3);
+		expect(userRequestLookup.contains(blistedUserOne)).toBe(false);
 	});
 });
--- assembly/index.ts
+++ assembly/index.ts
@@ -1,16 +1,28 @@
 import { Context } from "near-sdk-as";
-import { userLookup, accountLookup, onboardLookup, blockList } from "./model";
+import {
+	userLookup,
+	accountLookup,
+	onboardLookup,
+	blockList,
+	userRequestLookup,
+} from "./model";
 
 export function setUserInfo(username: string): u8 {
 	// Switching over strings is not yet supported
 	const uValid = validateUsername(username);
 	switch (uValid) {
+		// Username too short
 		case 2:
+		// Username already exists
 		case 3:
+		// Username too long
 		case 4:
+		// Username is blocklisted
 		case 7:
+		// Username contains invalid characters
 		case 8:
 			return uValid;
+		// Valid username
 		case 1:
 		default:
 			break;
@@ -18,9 +30,11 @@ export function setUserInfo(username: string): u8 {
 
 	const sender = Context.sender;
 	if (accountLookup.get(sender)) {
+		// accountId is already linked to a username
 		return 5;
 	}
 	if (!onboardLookup.contains(sender)) {
+		// accountId is not onboarded
 		return 6;
 	}
 
@@ -50,12 +64,15 @@ export function getAccountInfo(accountId: string): Array<string> | null {
 export function onboardAccount(accountId: string): u8 {
 	const sender = Context.sender;
 	if (sender != "capsule.testnet") {
+		// non-admin accounts are not allowed to onboard
 		return 0;
 	}
 	if (accountId.length < 2 || accountId.length > 64) {
+		// accountId length outside the permissible range
 		return 2;
 	}
 	if (onboardLookup.contains(accountId)) {
+		// accountId is already onboarded
 		return 3;
 	}
 	onboardLookup.set(accountId, true);
@@ -84,27 +101,119 @@ export function usernameInRange(username: string): bool {
 	return true;
 }
 
-export function validateUsername(username: string): u8 {
+export function validateUsername(
+	username: string,
+	blistcheck: bool = true
+): u8 {
 	if (username.length < 3) {
+		// Username too short
 		return 2;
 	}
 
 	if (username.length > 18) {
+		// Username too long
 		return 4;
 	}
 
 	if (!usernameInRange(username)) {
+		// Username contains invalid characters
 		return 8;
 	}
 
-	if (blockList.has(username) || username.includes("capsule")) {
+	if (blistcheck && (blockList.has(username) || username.includes("capsule"))) {
+		// Username is blocklisted
 		return 7;
 	}
 
 	const val = userLookup.get(username);
 	if (val) {
+		// Username already exists
 		return 3;
 	}
 
 	return 1;
+}
+
+export function requestSetUserInfo(username: string): u8 {
+	const uValid = validateUsername(username, false);
+	switch (uValid) {
+		// Username too short
+		case 2:
+		// Username already exists
+		case 3:
+		// Username too long
+		case 4:
+		// Username contains invalid characters
+		case 8:
+			return uValid;
+		// Username is blocklisted
+		case 7:
+		// Valid username
+		case 1:
+		default:
+			break;
+	}
+
+	const sender = Context.sender;
+	const publicKey = Context.senderPublicKey;
+
+	// accountId should not be associated with any existing username
+	if (accountLookup.get(sender)) {
+		return 5;
+	}
+
+	// To prevent spamming
+	if (!onboardLookup.contains(sender)) {
+		// accountId not onboarded
+		return 6;
+	}
+
+	// Reject usernames that are not in the blocklist
+	if (!blockList.has(username) && !username.includes("capsule")) {
+		// Username is not blocklisted
+		return 7;
+	}
+
+	const val = userRequestLookup.get(username);
+	if (!val) {
+		userRequestLookup.set(username, [sender, publicKey]);
+		// Successful userInfo update
+		return 1;
+	}
+
+	// Username already exists
+	return 3;
+}
+
+export function verifySetUserInfo(username: string): u8 {
+	const sender = Context.sender;
+	if (sender != "capsule.testnet") {
+		// Non-admin accounts are not allowed to verify
+		return 0;
+	}
+
+	const val = userRequestLookup.get(username);
+	if (!val) {
+		// Username doesn't exist in the waiting list
+		return 2;
+	}
+	userRequestLookup.delete(username);
+
+	const accountId = val[0];
+
+	if (accountLookup.get(accountId)) {
+		// NEAR account already linked to another username
+		return 5;
+	}
+
+	const val2 = userLookup.get(username);
+	if (!val2) {
+		userLookup.set(username, val);
+		accountLookup.set(accountId, username);
+		// Successful registration
+		return 1;
+	}
+
+	// Username already exists
+	return 3;
 }
